e462ac449fec5b397a978a494161a8b1b66f8913
wthornton18 <77442823+wthornton18@users.noreply.github.com>
2021-03-28 10:28:16 +0100

    Update scraper.py

--- Machine Learning Data/scraper.py
+++ Machine Learning Data/scraper.py
@@ -1,1 +1,13 @@
 
+import yfinance as yf
+assettype = ['currency', 'bond', 'commodity']
+for asset in assettype:
+    f = open(f'{asset}-symbols.txt', 'r')
+    assets = []
+    for line in f:
+        assets.append(line.strip())
+    f.close()
+    data = yf.download(assets, start='2005-1-1', end='2020-12-30')
+    data['Adj Close'].to_csv(f'{asset}-prices.csv')
+    data['Volume'].to_csv(f'{asset}-volumes.csv')
+
